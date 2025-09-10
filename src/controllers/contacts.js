fa85ddc07e94f6412b6ef83a251adc31c16d713c
kamila <sugakillersxs@gmail.com>
2025-09-10 15:10:32 +0200

    modificated

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,16 +1,16 @@
 import createHttpError from 'http-errors';
-
-
 import * as contactsServices from '../services/contacts.js';
 
 export const getAll = async (req, res) => {
   const contacts = await contactsServices.getAll();
 
   res.json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
-  });
+    status: "success",
+    code: 200,
+    data: {
+      contacts,
+  },
+});
 };
 
 export const getById = async (req, res) => {
@@ -22,9 +22,11 @@ export const getById = async (req, res) => {
   }
 
   res.json({
-    status: 200,
-    message: `Successfully found contact with id ${contactId}`,
-    data: contact,
+    status: "success",
+    code: 200,
+    data: {
+      contact,
+    },
   });
 };
 
@@ -32,9 +34,11 @@ export const create = async (req, res) => {
   const contact = await contactsServices.create(req.body);
 
   res.status(201).json({
-    status: 201,
-    message: 'Successfully created a contact!',
-    data: contact,
+    status: "success",
+    code: 201,
+    data: {
+      contact: contact,
+    },
   });
 };
 
@@ -47,9 +51,11 @@ export const update = async (req, res) => {
   }
 
   res.json({
-    status: 200,
-    message: 'Successfully patched a contact!',
-    data: contact,
+    status: "success",
+    code: 200,
+    data: {
+      contact: contact,
+    },
   });
 };
 
