37cf1e6844d83aede467ca176b9e702fb3fa8190
kamila <sugakillersxs@gmail.com>
2025-10-21 03:18:48 +0200

    Fixed filter

--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -1,4 +1,4 @@
-const parseContactType = (contactType) => {
+/*const parseContactType = (contactType) => {
   const isString = typeof contactType === 'string';
   if (!isString) return;
 
@@ -13,6 +13,7 @@ const parseIsFavourite = (isFavourite) => {
   return undefined;
 };
 
+
 export const parseFilterParams = (query) => {
   const { type, isFavourite } = query;
 
@@ -23,4 +24,36 @@ export const parseFilterParams = (query) => {
     type: parsedType,
     isFavourite: parsedIsFavourite,
   };
+};*/
+
+// src/utils/parseFilterParams.js
+
+const parseContactType = (contactType) => {
+  const isContactType = ['work', 'home', 'personal'];
+  if (typeof contactType === 'string' && isContactType.includes(contactType)) {
+    return contactType;
+  }
+};
+
+const parseIsFavourite = (isFavourite) => {
+  if (isFavourite === 'true') return true;
+  if (isFavourite === 'false') return false;
+};
+
+export const parseFilterParams = (query) => {
+  const { type, isFavourite } = query;
+  const filter = {};
+
+  const parsedType = parseContactType(type);
+  const parsedIsFavourite = parseIsFavourite(isFavourite);
+
+  if (parsedType) {
+    filter.contactType = parsedType;
+  }
+
+  if (parsedIsFavourite !== undefined) {
+    filter.isFavourite = parsedIsFavourite;
+  }
+
+  return filter;
 };
