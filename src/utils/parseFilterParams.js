b8d1842e4e5845199d022fb5457179c335ba6791
kamila <sugakillersxs@gmail.com>
2025-10-25 04:50:41 +0200

    modificated

--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -1,26 +1,29 @@
 const parseContactType = (contactType) => {
-  const isString = typeof contactType === 'string';
-  if (!isString) return;
-
-  const isContactType = (type) => ['work', 'home', 'personal'].includes(type);
-
-  if (isContactType(contactType)) return contactType;
+  const isContactType = ['work', 'home', 'personal'];
+  if (typeof contactType === 'string' && isContactType.includes(contactType)) {
+    return contactType;
+  }
 };
 
 const parseIsFavourite = (isFavourite) => {
   if (isFavourite === 'true') return true;
   if (isFavourite === 'false') return false;
-  return undefined;
 };
 
 export const parseFilterParams = (query) => {
   const { type, isFavourite } = query;
+  const filter = {};
 
   const parsedType = parseContactType(type);
   const parsedIsFavourite = parseIsFavourite(isFavourite);
 
-  return {
-    type: parsedType,
-    isFavourite: parsedIsFavourite,
-  };
+  if (parsedType) {
+    filter.contactType = parsedType;
+  }
+
+  if (parsedIsFavourite !== undefined) {
+    filter.isFavourite = parsedIsFavourite;
+  }
+
+  return filter;
 };
