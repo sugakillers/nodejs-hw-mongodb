218e064414e02404e596b81e770c6020def4dcf3
kamila <sugakillersxs@gmail.com>
2025-10-26 02:23:22 +0100

    add

--- src/server.js
+++ src/server.js
@@ -34,8 +34,7 @@ const __dirname = path.dirname(__filename);
       }),
   );
 
-
-  const swaggerDocument = YAML.load(path.resolve(__dirname, '../swagger/bundle.yaml'));
+  const swaggerDocument = YAML.load(path.resolve(__dirname, '../swagger/openapi.yaml'));
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use('/auth', authRouter);
